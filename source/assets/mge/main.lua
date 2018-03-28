abab16cdee8fdbc4a7f7c9ea28227a7e417b216b
Slavi Stoev <sl_stoev@abv.bg>
2018-03-28 05:43:56 +0200

    Removed some old stuff from version control.

--- source/assets/mge/main.lua
+++
@@ -1,9 +0,0 @@
-
-print("if you see me I am lua")
-
-function Initialize()
-	Start()
-end
-function Start()
-	Game.ShowHint("Start Message",400,800,20)
-end
--- source/code/myEngine/MyEngineConfig.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-namespace MyEngineConfig 
-{
-    const std::string MODEL_PATH ("myEngine/models/");
-    const std::string TEXTURE_PATH ("myEngine/textures/");
-    const std::string SHADER_PATH ("myEngine/shaders/");
-    const std::string FONT_PATH ("myEngine/fonts/");
-}
